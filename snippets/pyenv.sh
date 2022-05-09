715946e5c75a2b2eadd14321696c9ad80283b27b
Ra1ski <ra1almaty@gmail.com>
2022-05-09 14:55:40 +0600

    Snippents: Pyenv

---
+++ snippets/pyenv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# ------ Uncomment if you don't have Python installed
+#sudo apt-get install software-properties-common
+#sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt-get update
+#sudo apt-get install python10.8
+# -------
+
+ sudo apt-get install git python3-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl
+ sudo pip install virtualenvwrapper
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'pyenv virtualenvwrapper' >> ~/.bashrc
+
+exec $SHELL
